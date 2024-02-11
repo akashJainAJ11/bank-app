7bc8ba700c23825c7067fd3cd999ebd8a31b9003
akashJainAJ11 <akashjain100200@gmail.com>
2024-02-11 11:43:47 +0530

    Some update in Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,43 @@
-import { Appbar } from "../components/Appbar"
-import { Balance } from "../components/Balance"
-import { Users } from "../components/Users"
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+
+const GetUser = () => {
+    const [user, setUser] = useState({});
+    const [verify, setVerify] = useState(false);
+
+    useEffect(() => {
+        try {
+            axios
+                .get("https://bank-app-backend.vercel.app/api/v1/user/get", {
+                    headers: {
+                        Authorization: localStorage.getItem("token"),
+                    },
+                })
+                .then((res) => {
+                    setUser(res.data.user);
+                    setVerify(true);
+                });
+        } catch (error) {
+            console.error("Error fetching user data: ", error);
+        }
+    }, []);
+
+    return { user, verify };
+}
 
 export const Dashboard = () => {
-    return <div>
-        <Appbar />
-        <div className="m-8">
-            <Balance />
-            <Users />
+    const { user, verify } = GetUser();
+
+    return (
+        <div>
+            <Appbar />
+            <div className="m-8">
+                <Balance />
+                {verify && <Users userid={user._id} />}
+            </div>
         </div>
-    </div>
+    );
 }
